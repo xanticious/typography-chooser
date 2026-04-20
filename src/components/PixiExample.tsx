9011d28ea35badc861bb6adfca5e39261a8dd8bf
xanticious <TonyLitchfield@Hotmail.com>
2026-04-19 22:52:32 -0600

    Fixed typescript warning, added type checking.

--- src/components/PixiExample.tsx
+++ src/components/PixiExample.tsx
@@ -29,7 +29,7 @@ export default function PixiExample() {
 
     let onResize: () => void;
 
-    (async () => {
+    void (async () => {
       const container = containerRef.current;
       if (!container) return;
 
