4b1f89baaec1bf5f211b6e3192221012ba44f118
xanticious <TonyLitchfield@Hotmail.com>
2026-04-19 22:39:55 -0600

    Fixed github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // relative paths for GitHub Pages deployment
+  base: "/typography-chooser/",
   server: {
-    port: parseInt('4143', 10) || 5173,
+    port: parseInt("4143", 10) || 5173,
   },
 });
