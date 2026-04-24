d6d7b1d2502df91ce59770d1e6e426dc5df6bcba
xanticious <TonyLitchfield@Hotmail.com>
2026-04-23 19:35:52 -0600

    Fixed github pages.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/typography-chooser/",
+  base: process.env.GITHUB_ACTIONS ? "/typography-chooser/" : "./",
   server: {
     port: parseInt("4143", 10) || 5173,
   },
